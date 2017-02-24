5e1979c34a04ceab6be063cd0e9e4ba5f95df3e6
David <admin@dxscx.com>
2017-02-24 17:40:15 +0800

    feat: init project and add readme

---
+++ src/main/java/com/bjhetang/AccountManageApplication.java
@@ -0,0 +1,12 @@
+package com.bjhetang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManageApplication.class, args);
+	}
+}
